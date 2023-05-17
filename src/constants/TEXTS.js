1e071b5499d95c0450fdfb981f41ffc6483cd474
hozlucas28 <hozlucas28@gmail.com>
2023-05-17 10:20:04 -0300

    Create TEXTS.js constant

---
+++ src/constants/TEXTS.js
@@ -0,0 +1,35 @@
+export const TEXTS = {
+	form: {
+		labels: {
+			deposit: {
+				text: 'Initial deposit'
+			},
+			contribution: {
+				text: 'Annual contribution'
+			},
+			years: {
+				text: 'Years'
+			},
+			rate: {
+				text: 'Estimated interest'
+			}
+		},
+
+		inputs: {
+			errors: {
+				required: 'Required',
+				type: 'Must be a number',
+				min: 'The minimum value is 0',
+				max: 'The maximum value is 1'
+			}
+		},
+
+		submit: {
+			text: 'Calculate'
+		}
+	},
+
+	result: {
+		text: 'Final balance:'
+	}
+};
